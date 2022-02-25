13d8be5d18dd8007a8b7f21233e6be0301ade331
nishiyama <nishiyama@lvcap.jp>
2022-02-26 07:47:52 +0900

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by 西山好夫 on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
